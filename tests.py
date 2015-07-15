e94bdcec2cbd6f91311a011ad8974f7b030efade
João Miguel Neves <jneves@gosimpletax.com>
2015-07-15 22:19:14 +0100

    scaffold test file

---
+++ tests.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from unittest import TestCase, main
+from mock import Mock, patch
+
+
+class TestAuthentication(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestCurrencies(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestTicker(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestCard(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestContact(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestCurrencyPair(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestTransaction(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+class TestUser(TestCase):
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
