f1cb1af483a4074f5d36aaf9f50ce11ef081b5db
João Miguel Neves <jneves@gosimpletax.com>
2015-07-28 21:39:55 +0100

    update for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bitreserve',
+  packages = ['bitreserve'],
+  version = '0.1',
+  description = "Library for bitreserve.org's API",
+  author = 'João Miguel Neves',
+  author_email = 'joao@silvaneves.org',
+  url = 'https://github.com/jneves/bitreserve-python-sdk',
+  download_url = 'https://github.com/jneves/bitreserve-python-sdk/tarball/0.1',
+  keywords = ['bitreserve', 'currency', 'trading', 'api'],
+  classifiers = [],
+)
